e3f2138711b1e7c414f45ba1aa0933aa122e8a01
AfrinChakure-GSLab <afrin.chakure@gslab.com>
2018-05-08 08:26:36 +0530

    restful apis

---
+++ expressFiles/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+//root , callback function with 2 arguments
+app.get('/', (req, res) => {
+
+  res.send('helloworld');
+});
+// app.post();
+// app.put();
+// app.delete();
+
+app.get('/api/courses', (req, res) => {
+    res.send([1,2,3]);
+});
+
+app.listen(3000, () => console.log('listening on 3000'));
