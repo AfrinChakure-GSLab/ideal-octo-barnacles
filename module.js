b24c5c2a5716235832b99d44994221f63d4e7988
AfrinChakure-GSLab <afrin.chakure@gslab.com>
2018-05-08 08:36:49 +0530

    global objects and modules

---
+++ module.js
@@ -0,0 +1,10 @@
+
+// const logger = require('./logger'); //returns object exported\
+
+const log = require('./logger'); //returns object exported\
+
+
+// console.log(logger);
+
+// logger.log('message');
+log('message');
